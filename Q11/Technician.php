d8900ab1db8db22506702dd8e19a89ef53f33ba6
Cécile JEANNEAU <79309476+cecilejeanneau@users.noreply.github.com>
2023-04-04 15:01:17 +0200

    ✨Vehicle and Technician class with index test

---
+++ Q11/Technician.php
@@ -0,0 +1,61 @@
+<?php
+
+	/**
+	 * Summary of Technician
+	 */
+	class Technician {
+	/**
+	 * Summary of name
+	 * @var string
+	 * @var array 
+	 */
+		private string $name;
+		private $vehicles = [];
+
+		/**
+		 * Summary of __construct
+		 * @param mixed $name
+		 */
+		public function __construct(string $name) {
+			$this->name = $name;
+		}
+
+		/**
+		 * Summary of getName
+		 * @return string
+		 */
+		public function getName(): string {
+			return $this->name;
+		}
+
+		/**
+		 * Summary of addVehicle
+		 * @param Vehicle $vehicle
+		 * @return void
+		 */
+		public function addVehicle(Vehicle $vehicle): void {
+			$this->vehicles[] = $vehicle;
+		}
+
+		/**
+		 * Summary of removeVehicle
+		 * @param Vehicle $vehicle
+		 * @return void
+		 */
+		public function removeVehicle(Vehicle $vehicle): void {
+			$key = array_search($vehicle, $this->vehicles, true);
+			if ($key !== false) {
+				unset($this->vehicles[$key]);
+			}
+		}
+
+		/**
+		 * Summary of getVehicles
+		 * @return array<Vehicle>
+		 */
+		public function getVehicles(): array {
+			return $this->vehicles;
+		}
+
+	}
+?>
