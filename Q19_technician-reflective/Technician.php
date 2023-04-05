4caeae295c63dc1a0fde22db9b2c7e32ce6647be
Cécile JEANNEAU <79309476+cecilejeanneau@users.noreply.github.com>
2023-04-05 14:00:51 +0200

    🎉Technician reflective

---
+++ Q19_technician-reflective/Technician.php
@@ -0,0 +1,88 @@
+<?php
+
+	/**
+	 * Summary of Technician
+	 */
+	class Technician {
+
+		/**
+		 * Summary of subordinates
+		 * @var array
+		 */
+		private $subordinates = [];
+
+		/**
+		 * Summary of superior
+		 * @var
+		 */
+		private $superior = null;
+
+
+		/**
+		 * Summary of __construct
+		 * @param ?Technician $superior
+		 */
+		public function __construct(?Technician $superior) {
+			$this->superior = $superior;
+		}
+
+		/**
+		 * Summary of getSuperior
+		 * @return Technician
+		 */
+		public function getSuperior(): Technician {
+			return $this->superior;
+		}
+
+		/**
+		 * Summary of setSuperior
+		 * @param ?Technician $superior
+		 * @return void
+		 */
+		public function setSuperior(?Technician $superior): void {
+			if ($superior !== null && $superior->getSuperior() !== null) {
+				throw new Exception("A technician is supposed to have only one superior");
+			}
+			$this->superior = $superior;
+		}
+
+		/**
+		 * Summary of addSubordinate
+		 * @param Technician $subordinate
+		 * @return void
+		 */
+		public function addSubordinate(Technician $subordinate): void {
+			$this->subordinates[] = $subordinate;
+		}
+
+		/**
+		 * Summary of removeSubordinate
+		 * @param Technician $subordinate
+		 * @return void
+		 */
+		public function removeSubordinate(Technician $subordinate): void {
+			$index = array_search($subordinate, $this->subordinates, true);
+			if ($index !== false) {
+				array_splice($this->subordinates, $index, 1);
+			}
+		}
+
+		/**
+		 * Summary of getSubordinates
+		 * @return array<Technician>
+		 */
+		public function getSubordinates(): array {
+			return $this->subordinates;
+		}
+
+		/**
+		 * Summary of isSuperiorOf
+		 * @param Technician $subordinate
+		 * @return bool
+		 */
+		public function isSuperiorOf(Technician $subordinate): bool {
+			return $subordinate->getSuperior() === $this;
+		}
+	}
+
+?>
